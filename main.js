e15a2dd6021ce1390e89beabd66688d4ac2058a9
Lucas <lucas.norman007@gmail.com>
2023-08-24 15:50:40 +0200

    added navbar and better download buttons

---
+++ main.js
@@ -0,0 +1,16 @@
+// const apiURL = "https://api.github.com/repos/lucasNormanGitHub/build-and-measure-4.0-website/releases";
+
+// const downloadButton = document.querySelector("#download-button");
+// const downloadVersion = document.querySelector("#download-version");
+
+// fetch(apiURL).then(async response => {
+//     const json = await response.json();
+//     if (!response.ok) {
+//         alert(json.message);
+//         return;
+//     }
+//     const latestRelease = json[0];
+//     const latestInstallerURL = latestRelease.assets.find(asset => (/\.exe$/i).test(asset.name)).browser_download_url;
+//     downloadVersion.textContent = latestRelease.tag_name;
+//     downloadButton.href = latestInstallerURL;
+// });
