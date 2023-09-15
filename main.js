8087ea5b4f5cdbe6b4b75de70fef6f64de913a37
Lucas <lucas.norman007@gmail.com>
2023-09-15 18:12:55 +0200

    updated the hamburger icon in the navbar

--- main.js
+++ main.js
@@ -15,8 +15,9 @@
 //     downloadButton.href = latestInstallerURL;
 // });
 
-document.querySelector("#hamburger").onclick = () => {
+document.querySelector("#hamburger-button").onclick = () => {
     document.querySelector("#nav-links").classList.toggle("active");
+    document.querySelector("#hamburger-button").classList.toggle("active");
 };
 
 const toggleTableOfContentButton = document.querySelector("#toggle-table-of-content-button");
@@ -24,6 +25,7 @@ if (toggleTableOfContentButton) {
     const tableOfContent = document.querySelector("#table-of-content-wrapper");
     toggleTableOfContentButton.onclick = () => {
         tableOfContent.classList.toggle("active");
+        toggleTableOfContentButton.classList.toggle("active");
     };
 }
 const toggleListOfReleaseNotesButton = document.querySelector(
@@ -33,5 +35,6 @@ if (toggleListOfReleaseNotesButton) {
     const listOfReleaseNotes = document.querySelector("#list-of-release-notes-wrapper");
     toggleListOfReleaseNotesButton.onclick = () => {
         listOfReleaseNotes.classList.toggle("active");
+        toggleListOfReleaseNotesButton.classList.toggle("active");
     };
 }
