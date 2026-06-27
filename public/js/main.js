1992629a6c980ac8b9859d5aa9cc792783e1359e
Lucas <lucas.norman007@gmail.com>
2026-06-27 17:30:20 +0200

    improve the feedback navigation dropdown

--- public/js/main.js
+++ public/js/main.js
@@ -2,3 +2,8 @@ document.querySelector("#hamburger-button").onclick = () => {
     document.querySelector("#nav-links").classList.toggle("active");
     document.querySelector("#hamburger-button").classList.toggle("active");
 };
+
+document.querySelector(".faq-nav-link").onclick = () => {    
+    document.querySelector("#nav-links").classList.remove("active");
+    document.querySelector("#hamburger-button").classList.remove("active");
+};
